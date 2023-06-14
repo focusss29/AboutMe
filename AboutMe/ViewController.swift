0f21e83384be12eafd0d844f57f41e502fa4d85a
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-06-14 17:22:12 +0600

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Ilya Pogozhev on 14.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
